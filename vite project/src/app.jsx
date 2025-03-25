d6b096182fea90a33e9a363d8b44e617686d90f3
G1d3l <129759849+G1d3l@users.noreply.github.com>
2025-03-25 19:16:51 -0300

    Add files via upload

---
+++ vite project/src/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Bem-vindo ao meu app!</h1>
+      <p>Este é um exemplo básico de um aplicativo React.</p>
+    </div>
+  );
+}
+
+export default App;
