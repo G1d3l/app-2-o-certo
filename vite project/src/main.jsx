5defe783c84d0ae6f0de8ab5998d0aedc1f1afa4
G1d3l <129759849+G1d3l@users.noreply.github.com>
2025-03-25 20:23:21 -0300

    Update main.jsx

--- vite project/src/main.jsx
+++ vite project/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './App.css'; // Certifique-se de que o caminho do CSS está correto
+import './App.css'; 
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
